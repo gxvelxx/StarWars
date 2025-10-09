c41dd904eb3da2fafbcb941f13230edb2507a8aa
gxvelxx <wolf2005k@naver.com>
2025-10-09 12:41:16 +0900

    Refactoring
    Game_List<Enemy> refactoring
    Enemies.cs Add

---
+++ StarWars/Enemies.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars
+{
+    internal class Enemies
+    {
+        //적들을 담을 리스트
+        //마지막 적 생성 시간
+        //최대 적의 수, 생성간격
+        private List<Enemy> _enemies = new List<Enemy>();
+        private DateTime _lastSpawnTime;
+        private readonly int _maxEnemies;
+        private readonly double _spawnInterval;
+
+        //생성자에서 최대수,생성시간 관리
+        public Enemies(int maxEnemies, double spawnInterval)
+        {
+            _maxEnemies = maxEnemies;
+            _spawnInterval = spawnInterval;
+            _lastSpawnTime = DateTime.Now;
+        }
+
+        public void UpdateSpawn()
+        {
+            if (_enemies.Count <  _maxEnemies &&
+                (DateTime.Now - _lastSpawnTime).TotalSeconds >= _spawnInterval)
+            {
+                _enemies.Add(new Enemy());
+                _lastSpawnTime = DateTime.Now;
+            }
+        }
+
+        public void MoveAll()
+        {
+            foreach (Enemy enemy in _enemies)
+            {
+                enemy.MoveDown();
+            }
+        }
+
+        public void CreateAll()
+        {
+            foreach(Enemy enemy in _enemies)
+            {
+                enemy.Create();
+            }
+        }
+    }
+}
--- StarWars/Game.cs
+++ StarWars/Game.cs
@@ -15,53 +15,31 @@ namespace StarWars
         private Player player;
 
         //적
-        //private Enemy _enemy;
-        //여러 마리의 적들이 시간차를 두고 출몰해야함
-        //리스트에 담아야할까
-        private List<Enemy> enemies = new List<Enemy>();
-
-        //직전 적 생성 시간
-        private DateTime lastEnemySpawnTime;
-        //적의 최대수를 정해놓고
-        private const int maxEnemies = 16;           
+        private Enemies enemies;                 
 
         public Game()
         {
             gameover = false;
             player = new Player();
-            lastEnemySpawnTime = DateTime.Now;
+            enemies = new Enemies(16, 0.5);
         }
 
         //게임 시작
         public void Start()
         {
-            Console.CursorVisible = false;                                 
+            Console.CursorVisible = false;
 
             while (!gameover)
             {               
                 player.ReadKey();
                 player.Move();
 
-                //if문으로 돌려서 시간차로 생성하면 될듯
-                if (enemies.Count < maxEnemies &&
-                (DateTime.Now - lastEnemySpawnTime).TotalSeconds >= 0.8)
-                {
-                    enemies.Add(new Enemy());
-                    lastEnemySpawnTime = DateTime.Now;
-                }
-                //적들 이동처리
-                foreach (Enemy enemy in enemies)
-                {
-                    enemy.MoveDown();
-                }
+                enemies.UpdateSpawn();
+                enemies.MoveAll();
 
                 Console.Clear(); // 리랜더링 보이는게 거슬림
-
                 player.Create();
-                foreach (Enemy enemy in enemies)
-                {
-                    enemy.Create();
-                }
+                enemies.CreateAll();
 
                 Thread.Sleep(30);
             }
